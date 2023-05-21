9b72c99a8afebf5370ef4e12c298c061f91db907
Weizhi Li <liweizhismd@gmail.com>
2023-05-20 21:37:30 -0700

    Adding example utils for example

---
+++ src/main/java/ai/knowly/langtorch/example/ExampleUtils.java
@@ -0,0 +1,35 @@
+package ai.knowly.langtorch.example;
+
+import ai.knowly.langtorch.capability.Capability;
+import com.google.common.flogger.FluentLogger;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ExampleUtils {
+  private ExampleUtils() {}
+
+  static void readInputUntilEXIT(FluentLogger logger, Capability<String, String> capability) {
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+      String input;
+      final String sentinel = "EXIT"; // Define a sentinel value to exit the loop
+      logger.atInfo().log("Type '%s' and press Enter to exit the application.%n", sentinel);
+
+      while (true) {
+        input = reader.readLine();
+
+        if (input == null || sentinel.equalsIgnoreCase(input)) {
+          break; // Exit the loop if the user types the sentinel value
+        }
+
+        logger.atInfo().log("User: " + input);
+        String assistantMsg = capability.run(input);
+        logger.atInfo().log("Assistant: " + assistantMsg);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    logger.atInfo().log("Exiting the application.");
+  }
+}
--- src/main/java/ai/knowly/langtorch/example/SimpleChatBotWithoutMemory.java
+++ src/main/java/ai/knowly/langtorch/example/SimpleChatBotWithoutMemory.java
@@ -1,10 +1,9 @@
 package ai.knowly.langtorch.example;
 
+import static ai.knowly.langtorch.example.ExampleUtils.readInputUntilEXIT;
+
 import ai.knowly.langtorch.capability.module.openai.SimpleChatCapability;
 import com.google.common.flogger.FluentLogger;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class SimpleChatBotWithoutMemory {
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
@@ -12,31 +11,6 @@ public class SimpleChatBotWithoutMemory {
   public static void main(String[] args) {
     String openAIKey = "RandomKey";
     SimpleChatCapability chatBot = SimpleChatCapability.create(openAIKey);
-    readInputUntilEXIT(chatBot);
-  }
-
-  private static void readInputUntilEXIT(SimpleChatCapability chatBot) {
-    try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-      String input;
-      final String sentinel = "EXIT"; // Define a sentinel value to exit the loop
-
-      logger.atInfo().log("Type '%s' and press Enter to exit the application.%n", sentinel);
-
-      while (true) {
-        input = reader.readLine();
-
-        if (input == null || sentinel.equalsIgnoreCase(input)) {
-          break; // Exit the loop if the user types the sentinel value
-        }
-
-        logger.atInfo().log("User: " + input);
-        String assistantMsg = chatBot.run(input);
-        logger.atInfo().log("Assistant: " + assistantMsg);
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
-    logger.atInfo().log("Exiting the application.");
+    readInputUntilEXIT(logger, chatBot);
   }
 }

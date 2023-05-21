fc643700b48a613fd8777bf42a5f9676feb3fb82
Weizhi Li <liweizhismd@gmail.com>
2023-05-20 21:40:30 -0700

    Remove e.printStackTrace from example

--- src/main/java/ai/knowly/langtorch/example/ExampleUtils.java
+++ src/main/java/ai/knowly/langtorch/example/ExampleUtils.java
@@ -9,25 +9,23 @@ import java.io.InputStreamReader;
 public class ExampleUtils {
   private ExampleUtils() {}
 
-  static void readInputUntilEXIT(FluentLogger logger, Capability<String, String> capability) {
-    try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-      String input;
-      final String sentinel = "EXIT"; // Define a sentinel value to exit the loop
-      logger.atInfo().log("Type '%s' and press Enter to exit the application.%n", sentinel);
+  static void readInputUntilEXIT(FluentLogger logger, Capability<String, String> capability)
+      throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String input;
+    final String sentinel = "EXIT"; // Define a sentinel value to exit the loop
+    logger.atInfo().log("Type '%s' and press Enter to exit the application.%n", sentinel);
 
-      while (true) {
-        input = reader.readLine();
+    while (true) {
+      input = reader.readLine();
 
-        if (input == null || sentinel.equalsIgnoreCase(input)) {
-          break; // Exit the loop if the user types the sentinel value
-        }
-
-        logger.atInfo().log("User: " + input);
-        String assistantMsg = capability.run(input);
-        logger.atInfo().log("Assistant: " + assistantMsg);
+      if (input == null || sentinel.equalsIgnoreCase(input)) {
+        break; // Exit the loop if the user types the sentinel value
       }
-    } catch (IOException e) {
-      e.printStackTrace();
+
+      logger.atInfo().log("User: " + input);
+      String assistantMsg = capability.run(input);
+      logger.atInfo().log("Assistant: " + assistantMsg);
     }
 
     logger.atInfo().log("Exiting the application.");
--- src/main/java/ai/knowly/langtorch/example/SimpleChatBotWithoutMemory.java
+++ src/main/java/ai/knowly/langtorch/example/SimpleChatBotWithoutMemory.java
@@ -4,11 +4,12 @@ import static ai.knowly.langtorch.example.ExampleUtils.readInputUntilEXIT;
 
 import ai.knowly.langtorch.capability.module.openai.SimpleChatCapability;
 import com.google.common.flogger.FluentLogger;
+import java.io.IOException;
 
 public class SimpleChatBotWithoutMemory {
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     String openAIKey = "RandomKey";
     SimpleChatCapability chatBot = SimpleChatCapability.create(openAIKey);
     readInputUntilEXIT(logger, chatBot);

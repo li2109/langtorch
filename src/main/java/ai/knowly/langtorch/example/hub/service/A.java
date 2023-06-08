2223bc8cb1344c83a73512dd898ef0578544d48c
Weizhi Li <liweizhismd@gmail.com>
2023-06-07 19:37:50 -0700

    Remove hub example

--- src/main/java/ai/knowly/langtorch/example/hub/service/A.java
+++
@@ -1,9 +0,0 @@
-package ai.knowly.langtorch.example.hub.service;
-
-import ai.knowly.langtorch.hub.annotation.Torchlet;
-import ai.knowly.langtorch.hub.domain.TorchScope;
-import ai.knowly.langtorch.hub.domain.TorchScopeValue;
-
-@Torchlet
-@TorchScope(value = TorchScopeValue.PROTOTYPE)
-public class A {}
--- src/main/java/ai/knowly/langtorch/example/hub/service/B.java
+++
@@ -1,6 +0,0 @@
-package ai.knowly.langtorch.example.hub.service;
-
-import ai.knowly.langtorch.hub.annotation.Torchlet;
-
-@Torchlet
-public class B {}

f08e53a12708fa23da2512009e9f69b07ad83574
Weizhi Li <liweizhismd@gmail.com>
2023-06-16 00:22:50 -0700

    Adding OpenAITokenizer

---
+++ src/main/java/ai/knowly/langtorch/llm/openai/tokenization/Encodings.java
@@ -0,0 +1,30 @@
+package ai.knowly.langtorch.llm.openai.tokenization;
+
+import ai.knowly.langtorch.llm.openai.util.OpenAIModel;
+import com.google.common.collect.ImmutableMap;
+import com.knuddels.jtokkit.api.Encoding;
+import com.knuddels.jtokkit.api.EncodingRegistry;
+import com.knuddels.jtokkit.api.ModelType;
+
+public class Encodings {
+  private static final EncodingRegistry registry =
+      com.knuddels.jtokkit.Encodings.newDefaultEncodingRegistry();
+  public static final ImmutableMap<OpenAIModel, Encoding> ENCODING_BY_MODEL =
+      ImmutableMap.of(
+          OpenAIModel.GPT_3_5_TURBO,
+          registry.getEncodingForModel(ModelType.GPT_3_5_TURBO),
+          OpenAIModel.GPT_3_5_TURBO_16K,
+          registry.getEncodingForModel(ModelType.GPT_3_5_TURBO),
+          OpenAIModel.GPT_3_5_TURBO_0613,
+          registry.getEncodingForModel(ModelType.GPT_3_5_TURBO),
+          OpenAIModel.GPT_3_5_TURBO_16K_0613,
+          registry.getEncodingForModel(ModelType.GPT_3_5_TURBO),
+          OpenAIModel.GPT_4,
+          registry.getEncodingForModel(ModelType.GPT_4),
+          OpenAIModel.GPT_4_0613,
+          registry.getEncodingForModel(ModelType.GPT_4),
+          OpenAIModel.GPT_4_32K,
+          registry.getEncodingForModel(ModelType.GPT_4_32K),
+          OpenAIModel.GPT_4_32K_0613,
+          registry.getEncodingForModel(ModelType.GPT_4_32K));
+}
---
+++ src/main/java/ai/knowly/langtorch/llm/openai/tokenization/OpenAITokenizer.java
@@ -0,0 +1,68 @@
+package ai.knowly.langtorch.llm.openai.tokenization;
+
+import ai.knowly.langtorch.llm.openai.util.OpenAIModel;
+import ai.knowly.langtorch.schema.chat.ChatMessage;
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import java.util.Objects;
+
+/** Tokenizer for OpenAI models. */
+public class OpenAITokenizer {
+  private static final ImmutableList<OpenAIModel> GPT_3_MODELS =
+      ImmutableList.of(
+          OpenAIModel.GPT_3_5_TURBO,
+          OpenAIModel.GPT_3_5_TURBO_16K,
+          OpenAIModel.GPT_3_5_TURBO_0613,
+          OpenAIModel.GPT_3_5_TURBO_16K_0613);
+
+  private static final ImmutableList<OpenAIModel> GPT_4_MODELS =
+      ImmutableList.of(
+          OpenAIModel.GPT_4,
+          OpenAIModel.GPT_4_0613,
+          OpenAIModel.GPT_4_32K,
+          OpenAIModel.GPT_4_32K_0613);
+
+  public static List<Integer> encode(OpenAIModel model, String text) {
+    return Objects.requireNonNull(Encodings.ENCODING_BY_MODEL.get(model)).encode(text);
+  }
+
+  public static String decode(OpenAIModel model, List<Integer> tokens) {
+    return Objects.requireNonNull(Encodings.ENCODING_BY_MODEL.get(model)).decode(tokens);
+  }
+
+  public static long getTokenNumber(OpenAIModel model, String text) {
+    return encode(model, text).size();
+  }
+
+  // The algorithm for counting tokens is based on
+  // https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
+  public static long getTokenNumber(OpenAIModel model, List<ChatMessage> messages) {
+    int tokensPerMessage = 0;
+    int tokensPerName = 0;
+    if (GPT_3_MODELS.contains(model)) {
+      // Every message follows <|start|>{role/name}\n{content}<|end|>\n
+      tokensPerMessage = 4;
+      // If there's a name, the role is omitted
+      tokensPerName = -1;
+
+    } else if (GPT_4_MODELS.contains(model)) {
+      // Every message follows <|start|>{role/name}\n{content}<|end|>\n
+      tokensPerMessage = 3;
+      // If there's a name, the role is omitted
+      tokensPerName = 1;
+    } else {
+      throw new UnsupportedOperationException("You model is not supported yet for token counting.");
+    }
+
+    int numberOfTokens = 0;
+    for (ChatMessage message : messages) {
+      numberOfTokens += tokensPerMessage;
+      numberOfTokens += encode(model, message.getContent()).size();
+      numberOfTokens += encode(model, message.getRole().name()).size();
+      numberOfTokens += encode(model, message.getName()).size() + tokensPerName;
+    }
+    // Every reply is primed with <|start|>assistant<|message|>
+    numberOfTokens += 3;
+    return numberOfTokens;
+  }
+}

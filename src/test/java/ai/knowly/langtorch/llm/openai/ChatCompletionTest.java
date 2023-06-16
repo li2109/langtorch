b630677421c9dcd816402aa2d223ad2cc3a971bd
Weizhi Li <liweizhismd@gmail.com>
2023-06-15 22:02:42 -0700

    Add missing test with replaced open ai service config module

--- src/test/java/ai/knowly/langtorch/llm/openai/ChatCompletionTest.java
+++ src/test/java/ai/knowly/langtorch/llm/openai/ChatCompletionTest.java
@@ -1,24 +1,33 @@
 package ai.knowly.langtorch.llm.openai;
 
-import static ai.knowly.langtorch.util.OpenAIServiceTestingUtils.OPENAI_TESTING_SERVICE;
 import static org.junit.jupiter.api.Assertions.*;
 
 import ai.knowly.langtorch.llm.openai.schema.dto.completion.chat.ChatCompletionChoice;
 import ai.knowly.langtorch.llm.openai.schema.dto.completion.chat.ChatCompletionRequest;
 import ai.knowly.langtorch.schema.chat.ChatMessage;
 import ai.knowly.langtorch.schema.chat.SystemMessage;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.testing.fieldbinder.BoundFieldModule;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIf;
 
 @EnabledIf("ai.knowly.langtorch.util.TestingSettingUtils#enableOpenAILLMServiceLiveTrafficTest")
 class ChatCompletionTest {
+  @Inject private OpenAIService service;
+
+  @BeforeEach
+  void setUp() {
+    Guice.createInjector(BoundFieldModule.of(this), new OpenAIServiceConfigTestingModule())
+        .injectMembers(this);
+  }
 
   @Test
   void createChatCompletion() {
-    OpenAIService service = OPENAI_TESTING_SERVICE;
     final List<ChatMessage> messages = new ArrayList<>();
     messages.add(SystemMessage.of("You are a dog and will speak as such."));
 

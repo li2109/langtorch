b98bfb574df2c767948786dd231f126da68986f2
ayoola adedeji <ayoola.adedeji@outlook.com>
2023-06-18 12:25:28 +0100

    removed unused import

--- src/main/java/ai/knowly/langtorch/connector/pdf/PDFConnector.java
+++ src/main/java/ai/knowly/langtorch/connector/pdf/PDFConnector.java
@@ -4,7 +4,6 @@ import ai.knowly.langtorch.connector.Connector;
 import com.google.common.flogger.FluentLogger;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Optional;
 import javax.inject.Inject;

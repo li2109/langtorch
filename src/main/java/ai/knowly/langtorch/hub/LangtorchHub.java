50dc8a2c2d2f3f982cbccc185bd3dcc1f4b4b5e4
Weizhi Li <liweizhismd@gmail.com>
2023-06-21 21:48:41 -0700

    Adding getInstance by class type and annotation in langtorch hub

--- src/main/java/ai/knowly/langtorch/hub/LangtorchHub.java
+++ src/main/java/ai/knowly/langtorch/hub/LangtorchHub.java
@@ -4,6 +4,8 @@ import ai.knowly.langtorch.hub.module.token.TokenUsage;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
+import com.google.inject.Key;
+import java.lang.annotation.Annotation;
 import javax.inject.Inject;
 
 /** LangtorchHub is the entry point for the Langtorch library. */
@@ -17,6 +19,10 @@ public class LangtorchHub {
 
   public <T> T getInstance(Class<T> clazz) {
     return injector.getInstance(clazz);
+  }
+
+  public <T> T getInstance(Class<T> clazz, Class<? extends Annotation> annotation) {
+    return injector.getInstance(Key.get(clazz, annotation));
   }
 
   public TokenUsage getTokenUsage() {
